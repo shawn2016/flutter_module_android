54723c6895c567e802e1d5597263c4575235f421
shawn <>
2025-11-24 21:11:27 +0800

    feat:使用aar

--- android_simple/app/src/main/java/com/simple/app/MainActivity.kt
+++ android_simple/app/src/main/java/com/simple/app/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.simple.app
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnOpenFlutter = findViewById<Button>(R.id.btnOpenFlutter)
+        btnOpenFlutter.setOnClickListener {
+            // 启动 Flutter 页面
+            startActivity(
+                FlutterActivity
+                    .withNewEngine()
+                    .initialRoute("/")
+                    .build(this)
+            )
+        }
     }
 }
 
